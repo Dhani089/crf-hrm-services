c87d29a484c7b5f0b9bfd4ffeb18c24759604d21
Dhani089 <109782129+Dhani089@users.noreply.github.com>
2022-08-09 20:34:48 +0530

    Delete hrm-services/employeeservice directory

--- hrm-services/employeeservice/employeeservice/src/main/java/com/emp/employeeservice/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emp.employeeservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.emp.employeeservice.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
